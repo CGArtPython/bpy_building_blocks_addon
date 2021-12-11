6c2b9fddc3bfb17453f5c7d4c66f6cac1267757c
Victor Stepanov <victor.a.stepanov@gmail.com>
2021-12-11 14:15:53 -0800

    bpybb addon version v0.0.1 changes: * setting the version on 0.0.1 * adding a setup.py to create the package * adding the following functions to utils.py: active_object, make_active, editmode, purge_orphans, clean_scene * adding the following function to random.py: time_seed * adding functions that set the output render resolution in output.py * adding the following functions to object.py: apply_location, add_empty, track_empty * adding the following functions to material.py: apply_material, make_color_ramp_from_color_list * adding the following function to mesh.py: get_vert_coordinates_list * adding the following function to hdri.py: apply_hdri * adding the following function to collection.py: create_collection * adding add-on enabling functions for "Add Curve Extra Objects", "Add Mesh Extra Objects", "ANT Landscape", "Modifier Tools", "Images as Planes", "Pointcache (pc2)" * update license text to use a real name * adding .gitignore * adding a basic README.md

--- __init__.py
+++ __init__.py
@@ -1,18 +1,19 @@
 import os
+import sys
 
 ADDON_FOLDER_PATH = os.path.dirname(__file__)
-VERSION = (0, 0, 0)
+VERSION = (0, 0, 1)
 MODULE_NAME = "bpybb"
 ADDON_NAME = (
     f"[{MODULE_NAME}] bpy Building Blocks v{VERSION[0]}.{VERSION[1]}.{VERSION[2]}"
 )
 
 bl_info = {
-    "name": "[bpybb] bpy Building Blocks v0.0.0",
+    "name": "[bpybb] bpy Building Blocks v0.0.1",
     "author": "Viktor Stepanov",
-    "version": (0, 0, 0),
+    "version": (0, 0, 1),
     "blender": (2, 83, 0),
-    "description": "Helper functions",
+    "description": "A collection of helper functions and code used for speeding up Blender 3D Python script development.",
     "category": "Development",
     "doc_url": "https://github.com/CGArtPython/bpy_building_blocks_addon",
 }
@@ -21,9 +22,15 @@ bl_info = {
 def register():
     print(f'ENABLED "{ADDON_NAME}" addon')
 
+    print(f"\tadding {MODULE_NAME} to sys path: {ADDON_FOLDER_PATH}")
+    sys.path.append(ADDON_FOLDER_PATH)
+
 
 def unregister():
     print(f'DISABLE "{ADDON_NAME}" addon')
+
+    print(f"\tremoving {MODULE_NAME} from sys path: {ADDON_FOLDER_PATH}")
+    sys.path.remove(ADDON_FOLDER_PATH)
 
 
 if __name__ == "__main__":
