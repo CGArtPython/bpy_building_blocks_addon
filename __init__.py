5e00386c90f4ea0d4fb13e2a27244898840d1d8a
Victor Stepanov <victor.a.stepanov@gmail.com>
2022-08-20 20:09:40 -0700

    bpybb addon version v0.0.21

--- __init__.py
+++ __init__.py
@@ -2,16 +2,16 @@ import os
 import sys
 
 ADDON_FOLDER_PATH = os.path.dirname(__file__)
-VERSION = (0, 0, 2)
+VERSION = (0, 0, 21)
 MODULE_NAME = "bpybb"
 ADDON_NAME = (
     f"[{MODULE_NAME}] bpy Building Blocks v{VERSION[0]}.{VERSION[1]}.{VERSION[2]}"
 )
 
 bl_info = {
-    "name": "[bpybb] bpy Building Blocks v0.0.2",
+    "name": "[bpybb] bpy Building Blocks v0.0.21",
     "author": "Viktor Stepanov",
-    "version": (0, 0, 2),
+    "version": (0, 0, 21),
     "blender": (2, 83, 0),
     "description": "A collection of helper functions and code used for speeding up Blender 3D Python script development.",
     "category": "Development",
